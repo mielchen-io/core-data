b457201690d4b68fbe3096c5d1005be4c6c64329
Marvin Mielchen <100961250+marvinmielchen@users.noreply.github.com>
2025-07-13 00:34:28 +0200

    -

--- src/write_ahead_log/mod.rs
+++ src/write_ahead_log/mod.rs
@@ -1,2 +1,2 @@
 pub mod write_ahead_log;
-pub mod segment_wal;
+pub mod copy_wal;
--- tests/write_ahead_log_test.rs
+++ tests/write_ahead_log_test.rs
@@ -1,8 +1,10 @@
-use core_data::write_ahead_log::segment_wal::SegmentWal;
+use core_data::write_ahead_log::copy_wal::{CopyWal};
 
 #[test]
 fn test_segment_wal() {
     let temp_dir = tempfile::tempdir().expect("failed to create temp dir");
     let temp_path = temp_dir.path();
+
+
 
 }
