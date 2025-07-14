9bb1c3bff558ca20ce4b1a0c7d7919b2c28cf445
Marvin Mielchen <100961250+marvinmielchen@users.noreply.github.com>
2025-07-14 19:43:51 +0200

    -

--- src/write_ahead_log/simple_wal_recovery.rs
+++ src/write_ahead_log/simple_wal_recovery.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{Read, Seek}, path::PathBuf};
+use std::{fs::File, io::{self, Read, Seek, SeekFrom, Write}, path::PathBuf};
 
 use crate::write_ahead_log::simple_wal::SimpleWal;
 
@@ -20,10 +20,10 @@ impl SimpleWal {
         assert!(dir_path.join("wal.meta").exists(), "wal.meta does not exist in directory: {:?}", dir_path);
 
         let tick_file_path = dir_path.join("wal.tick");
-        let tick_file = open_file_with_permissions(&tick_file_path);
+        let mut tick_file = open_file_with_permissions(&tick_file_path);
 
         let tock_file_path = dir_path.join("wal.tock");
-        let tock_file = open_file_with_permissions(&tock_file_path);
+        let mut tock_file = open_file_with_permissions(&tock_file_path);
 
         let log_file_path = dir_path.join("wal.log");
         let log_file = open_file_with_permissions(&log_file_path);
@@ -44,16 +44,48 @@ impl SimpleWal {
         
         if valid_indicator && log_file_len > 0 {
             // If the operational file indicator is valid but the log file is not empty, do a Recovery Type A
-            // 1. Copy the the fallback to the operational file
-            let (operational_file, fallback_file) = self.get_operational_and_fallback_file();
-            
-            io::copy(fallback_file, operational_file)
-                .expect("Failed to copy fallback file to operational file");
-            operational_file.sync_all()
-                .expect("Failed to sync operational file after copying fallback");
+             // 1. Copy the the fallback to the operational file
+            let operational_file: &mut File;
+            let fallback_file: &mut File;
+            if operational_file_indicator.iter().all(|&x| x == 0) {
+                operational_file = &mut tick_file;
+                fallback_file = &mut tock_file;
+            } else {
+                operational_file = &mut tock_file;
+                fallback_file = &mut tick_file;
+            }
+            operational_file
+                .seek(SeekFrom::Start(0))
+                .expect("Failed to seek in operational file");
+            fallback_file
+                .seek(SeekFrom::Start(0))
+                .expect("Failed to seek in fallback file");
+            operational_file.set_len(0).expect("Failed to erase operational file");
+            io::copy(fallback_file, operational_file).expect("Failed to copy fallback file to operational file");
+            operational_file.sync_all().expect("Failed to sync operational file");
+            // 2. Erase the log file
+            log_file.set_len(0).expect("Failed to erase log file");
+            log_file.sync_all().expect("Failed to sync log file");
         } else if !valid_indicator && dir_path.join("wal.tick").exists() {
-            // If the operational file indicator is not valid and wal.tick exists, do a Recovery Type B
-            todo!("Recovery Type B: Implement the recovery logic for type B");
+            // If the operational file indicator is not valid, do a Recovery Type B
+            // 1. Copy the tock file to the tick file
+            tock_file
+                .seek(SeekFrom::Start(0))
+                .expect("Failed to seek in tock file");
+            tick_file
+                .seek(SeekFrom::Start(0))
+                .expect("Failed to seek in tick file");
+            tick_file.set_len(0).expect("Failed to erase tick file");
+            io::copy(&mut tock_file, &mut tick_file).expect("Failed to copy tock file to tick file");
+            tick_file.sync_all().expect("Failed to sync tick file");
+            // 2. Update the operational file indicator in the meta file (write all ones)
+            meta_file
+                .seek(SeekFrom::Start(0))
+                .expect("Failed to seek in meta file");
+            meta_file
+                .write_all(&[1u8; 32])
+                .expect("Failed to write operational file indicator");
+            meta_file.sync_all().expect("Failed to sync meta file");
         } 
 
         return Self {
@@ -64,25 +96,8 @@ impl SimpleWal {
         };
     }
 
-    fn get_operational_and_fallback_file(&mut self) -> (&File, &File) {
-        self.meta_file
-            .seek(std::io::SeekFrom::Start(0))
-            .expect("Failed to seek in meta file");
-        let mut operational_file_indicator: [u8; 32] = [0; 32];
-        self.meta_file
-            .read_exact(&mut operational_file_indicator)
-            .expect("Failed to read operational file indicator");
-        if operational_file_indicator.iter().all(|&x| x == 0) {
-            return (&self.tick_file, &self.tock_file);
-        } else if operational_file_indicator.iter().all(|&x| x == 1) {
-            return (&self.tock_file, &self.tick_file);
-        } else {
-            panic!("Invalid operational file indicator");
-        }
-    }
+   
 }
-
-
 
 fn open_file_with_permissions(path: &PathBuf) -> std::fs::File {
     std::fs::OpenOptions::new()
